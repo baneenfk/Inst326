f43046658a92b238d567ed14eb9e6d22e2c8fbe6
Christian Perhach <cjperhach1@gmail.com>
2021-04-01 13:16:06 -0400

    Created the Final_Project.py and added some classes and functions

---
+++ Final_Project.py
@@ -0,0 +1,10 @@
+class credit_card(): 
+    def __init__(self):
+        
+    def extract: 
+        
+class statistical_computaions(): 
+    def mean(): 
+    def standard_deviation():
+    def confidence_interal():
+    def relative_entropy():
