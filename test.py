9e11308d8005e7813b2f9bea742d0f8233049a5c
Christian Perhach <cjperhach1@gmail.com>
2021-05-14 17:40:22 -0400

    added to pytest

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
+import Final_Project as fp 
 import pytest
-import Final_Project 
-import math
 
-def irregular_times_count():
-    
-=======
-import pytest
-import Final_Project 
-import math
-
-def irregular_times_count():
-    
->>>>>>> fc5c13e46de3e155c959c12f9328cadcd58d14c2
-    assert math.isclose(irregular_times_count(0),0)
+def user():
+    #Test
+    assert fp.user(Jeff) == Jeff
+def irregular_times():
+    #Test
+    assert fp.irregular_times_count(0) == 1 
+def irregular_amount():
+    #Test
+    assert fp.irregular_amount(650) == 1
