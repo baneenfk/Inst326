6d8eaf1d4cd162c8fd65153be5f33c2373665e7d
Christian Perhach <cjperhach1@gmail.com>
2021-05-14 17:49:20 -0400

    added to pytest

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+import pytest 
 import Final_Project as fp 
-import pytest
 
-def user():
+
+def test_user():
     #Test
     assert fp.user(Jeff) == Jeff
-def irregular_times():
+def test_irregular_times():
     #Test
     assert fp.irregular_times_count(0) == 1 
-def irregular_amount():
+def test_irregular_amount():
     #Test
     assert fp.irregular_amount(650) == 1
