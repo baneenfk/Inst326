662f6e6b4b63830dc0ca13add52913a8da41ddb7
Christian Perhach <cjperhach1@gmail.com>
2021-05-15 17:25:17 -0400

    The changes from lastnight

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
 import pytest 
 import Final_Project as fp 
 
+@pytest.fixture
+def credit_card():
+    return fp.Credit_Card_Holder("fraudTest1.csv")
 
-def test_user():
+def test_user(credit_card):
     #Test
-    assert fp.user(Jeff) == Jeff
-def test_irregular_times():
+    x = credit_card.user("Jeff", "Elliott")
+    assert (x['first'] == "Jeff").all()
+    assert (x['last'] == "Elliott").all()
+    assert len(x) == 20 
+    
+def test_irregular_times(credit_card):
     #Test
-    assert fp.irregular_times_count(0) == 1 
-def test_irregular_amount():
+    credit_card.user("Aaron", "Murray")
+    assert credit_card.irregular_times() == 22
+    credit_card.user("Joseph", "Murray")
+    assert credit_card.irregular_times() == 18
+    
+def test_irregular_amount(credit_card):
     #Test
-    assert fp.irregular_amount(650) == 1
+    credit_card.user("Adam", "Riddle")
+    assert credit_card.irregular_amount() == 0 
+    credit_card.user("Jeff", "Elliott")
+    assert credit_card.irregular_amount() == 1
