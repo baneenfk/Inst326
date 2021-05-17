ced97292a86f513b9a83b7923dafc82318971946
Christian Perhach <cjperhach1@gmail.com>
2021-05-16 22:50:12 -0400

    test.py

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ def credit_card():
     return fp.Credit_Card_Holder("fraudTest1.csv")
 
 def test_user(credit_card):
-    #Test
+    """ Does credit_card.user() check to see if the first name and last name are 
+        in the CSV, does it return the last 20 transactions, and does it raise 
+        the correct error if first name and last name are not in the CSV?
+    """
     x = credit_card.user("Jeff", "Elliott")
     assert (x['first'] == "Jeff").all()
     assert (x['last'] == "Elliott").all()
@@ -17,14 +20,18 @@ def test_user(credit_card):
         credit_card.user("7",("1fjjf"))
     
 def test_irregular_times(credit_card):
-    #Test
+    """ Does credit_card.irregular_times() return the correct count of 
+        transactions that occured at irregular hours. 
+    """
     credit_card.user("Aaron", "Murray")
     assert credit_card.irregular_times() == 22
     credit_card.user("Joseph", "Murray")
     assert credit_card.irregular_times() == 18
     
 def test_irregular_amount(credit_card):
-    #Test
+    """ Does credit_card.irregular_amount() return the correct count of 
+        transactions that had an irregular_amount. 
+    """
     credit_card.user("Adam", "Riddle")
     assert credit_card.irregular_amount() == 0 
     credit_card.user("Jeff", "Elliott")
